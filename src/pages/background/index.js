727b26a112f991bb97fbfbe6d5e9ac8b0de73f96
Theo Dao <trungdao912@gmail.com>
2023-08-18 15:11:03 +0700

    feat: adding chrome extension sidebar

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -1,62 +1,35 @@
-// let user_signed_in = false
-
-// const CLIENT_ID = encodeURIComponent(
-//   '349249349340-u6pjg3g4j3951oggpbidvj0oiisep78d.apps.googleusercontent.com',
-// )
-// const RESPONSE_TYPE = encodeURIComponent('id_token')
-// const REDIRECT_URI = encodeURIComponent(
-//   'https://' + chrome.runtime.id + '.chromiumapp.org',
-// )
-// const STATE = encodeURIComponent('jfkls3n')
-// const SCOPE = encodeURIComponent('openid')
-// const PROMPT = encodeURIComponent('consent')
-
-// function create_oauth2_url() {
-//   let nonce = encodeURIComponent(
-//     Math.random().toString(36).substring(2, 15) +
-//       Math.random().toString(36).substring(2, 15),
-//   )
-//   let url = `https://accounts.google.com/o/oauth2/v2/auth
-//   ?client_id=${CLIENT_ID}
-//   &response_type=${RESPONSE_TYPE}
-//   &redirect_uri=${REDIRECT_URI}
-//   &state=${STATE}
-//   &scope=${SCOPE}
-//   &prompt=${PROMPT}
-//   &nonce=${nonce}`
-//   console.log(url)
-//   return url
-// }
-
-// function is_user_signed_in() {
-//   return user_signed_in
-// }
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.message === 'login') {
-//     if (is_user_signed_in()) {
-//       console.log('user is already signed in.')
-//     } else {
-//       chrome.identity.launchWebAuthFlow(
-//         {
-//           url: create_oauth2_url(),
-//           interactive: true,
-//         },
-//         function (redirect_uri) {
-//           console.log(redirect_uri)
-//           sendResponse('success')
-//         },
-//       )
-
-//       return true
-//     }
-//   } else if (request.message === 'logout') {
-//   }
-// })
-
 const API_KEY = 'AIzaSyDdJpoy59g2F9gnzLMOb635KhCGZqCIilA'
 
 let user_signed_in = false
+
+function injectedFunction() {
+  var iframe = document.createElement('iframe')
+  iframe.style.background = 'transparent'
+  iframe.style.position = 'fixed'
+  iframe.style.top = '0px'
+  iframe.style.right = '0px'
+  iframe.style.zIndex = '9000000000000000000'
+  iframe.style.border = '1px solid rgba(155, 155, 155, 0.18)'
+  iframe.style.borderTopLeftRadius = '10px'
+  iframe.style.borderBottomLeftRadius = '10px'
+  iframe.style.boxShadow = 'rgba(0, 0, 0, 0.35) 0px 5px 15px'
+  iframe.src = chrome.runtime.getURL('popup.html')
+  iframe.style.width = '450px'
+  iframe.style.height = '100%'
+
+  document.body.appendChild(iframe)
+
+  document.body.addEventListener('click', () => {
+    iframe.style.width = '0px'
+  })
+}
+
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id, allFrames: true },
+    func: injectedFunction,
+  })
+})
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'login') {
