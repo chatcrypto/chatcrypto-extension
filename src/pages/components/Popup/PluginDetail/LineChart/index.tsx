b3e27d4b0b116bb19115efe2a7bc05e22e808c70
carras16 <quynguyenbk98@gmail.com>
2023-09-07 15:28:29 +0700

    fix: chart line

--- src/pages/components/Popup/PluginDetail/LineChart/index.tsx
+++ src/pages/components/Popup/PluginDetail/LineChart/index.tsx
@@ -10,7 +10,7 @@ import {
   Tooltip,
 } from 'chart.js'
 import zoomPlugin from 'chartjs-plugin-zoom'
-import { Line } from 'react-chartjs-2'
+import { Line, Scatter } from 'react-chartjs-2'
 
 import { Button, Flex, rem } from '@mantine/core'
 import { IconRefresh } from '@tabler/icons-react'
@@ -22,45 +22,71 @@ import {
 import { COLORS_CHART } from '../constants'
 import { checkType2ParseData } from '../ultils.chart'
 
+// ChartJS.register(
+//   zoomPlugin,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// )
+
 ChartJS.register(
   zoomPlugin,
-  CategoryScale,
   LinearScale,
   PointElement,
   LineElement,
-  Title,
   Tooltip,
   Legend,
+  Title,
 )
 
 const LineChart = ({ pluginDetail }: { pluginDetail: IPluginDetail }) => {
-  const scaleOpts = {
-    grid: {
-      // borderColor: Utils.randomColor(1),
-      color: 'rgba( 0, 0, 0, 0.1)',
-    },
-    title: {
-      display: true,
-      // text: (ctx) => ctx.scale.axis + ' axis',
-    },
-  }
-  const scales: any = {
-    x: {
-      type: 'category',
-      min: 1,
-      max: 11,
-    },
-    y: {
-      type: 'linear',
-    },
-  }
-  Object.keys(scales).forEach((scale) =>
-    Object.assign(scales[scale], scaleOpts),
-  )
+  // const scaleOpts = {
+  //   grid: {
+  //     // borderColor: Utils.randomColor(1),
+  //     color: 'rgba( 0, 0, 0, 0.1)',
+  //   },
+  //   title: {
+  //     display: true,
+  //     // text: (ctx) => ctx.scale.axis + ' axis',
+  //   },
+  // }
+  // const scales: any = {
+  //   x: {
+  //     type: 'category',
+  //     min: 1,
+  //     max: 11,
+  //   },
+  //   y: {
+  //     type: 'linear',
+  //   },
+  // }
+  // Object.keys(scales).forEach((scale) =>
+  //   Object.assign(scales[scale], scaleOpts),
+  // )
+
+  const chartData: IDateLineChart[] = useMemo(() => {
+    return pluginDetail.data as IDateLineChart[]
+  }, [pluginDetail])
+
   const options = useMemo(() => {
     return {
       responsive: true,
-      scales: scales,
+      // scales: scales,
+      scales: {
+        x: {
+          ticks: {
+            // Include a dollar sign in the ticks
+            callback: function (value: any, index: number, values: any) {
+              console.log(value, index, values)
+              return checkType2ParseData(chartData[0].x_field, value)
+            },
+          },
+        },
+      },
       plugins: {
         legend: {
           position: 'top' as const,
@@ -92,34 +118,24 @@ const LineChart = ({ pluginDetail }: { pluginDetail: IPluginDetail }) => {
         },
       },
     }
-  }, [pluginDetail.title, scales])
-
-  // const labels = [
-  //   'January',
-  //   'February',
-  //   'March',
-  //   'April',
-  //   'May',
-  //   'June',
-  //   'July',
-  // ]
-
-  const chartData: IDateLineChart[] = useMemo(() => {
-    return pluginDetail.data as IDateLineChart[]
-  }, [pluginDetail])
+  }, [pluginDetail.title, chartData])
 
   const data = useMemo(() => {
     return {
-      labels: chartData[0].row_data.map((d) =>
-        checkType2ParseData(chartData[0].x_field, d[chartData[0].x_field]),
-      ),
       datasets: chartData.map((line, index) => ({
         label: line.label,
-        data: line.row_data.map((point) => point[chartData[0].y_field]),
+        data: line.row_data.map((point) => ({
+          x: point[line.x_field],
+          y: point[line.y_field],
+        })),
+        showLine: true,
+        fill: false,
         backgroundColor: COLORS_CHART[index],
       })),
     }
   }, [pluginDetail])
+
+  console.log(data, 'data')
 
   const chartRef = useRef(null)
   const handleResetZoom = () => {
@@ -129,9 +145,11 @@ const LineChart = ({ pluginDetail }: { pluginDetail: IPluginDetail }) => {
   }
 
   return (
-    <Flex direction='column' align='flex-end'>
-      <Button onClick={handleResetZoom} variant='outline'><IconRefresh size={rem(20)}/></Button>
-      <Line options={options as any} data={data} ref={chartRef}/>
+    <Flex direction="column" align="flex-end">
+      <Button onClick={handleResetZoom} variant="outline">
+        <IconRefresh size={rem(20)} />
+      </Button>
+      <Scatter options={options as any} data={data} ref={chartRef} />
     </Flex>
   )
 }
